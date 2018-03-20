3ff5603feed9c9a44eae8b3999ca296b91e57a53
kamilkhan@rediffmail.com <donkhan>
2018-03-20 10:06:34 +0530

    Hard coded as of now

--- src/auth.py
+++ src/auth.py
@@ -1,10 +1,10 @@
 import requests
-import c
 
 
 def auth(site,user_name,pass_word):
     (u,p) = (user_name,pass_word)
-    r = requests.post(site + "/sessions/current", data=c.credentials, verify=False, auth=(u,p))
+    r = requests.post(site + "/sessions/current", data={"username":'sa@maxmoney.com',"password" : 'MaxMoney@2016'},
+                      verify=False, auth=('maxmoney','maxmoney@@1'))
     if r.status_code == 200:
         return r.json().get('session')
     if r.status_code == 201:
