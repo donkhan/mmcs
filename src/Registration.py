b8824a86553b71c9068c871329c58e522367a73c
kamilkhan@rediffmail.com <donkhan>
2018-04-11 08:13:52 +0530

    Printing the image folder

--- src/Registration.py
+++ src/Registration.py
@@ -14,7 +14,7 @@ class Registration:
 
     def done(self):
         logging.debug("Going to delete %s",self.folder)
-        shutil.rmtree(self.folder)
+        #shutil.rmtree(self.folder)
         logging.debug("Deleted %s",self.folder)
 
 
