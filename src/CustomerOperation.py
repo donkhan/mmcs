1e97ac5c2eb8467a27a39a02f9a226b2d38ce3a0
kamilkhan@rediffmail.com <donkhan>
2018-04-11 08:10:12 +0530

    Only 2 operations

--- src/CustomerOperation.py
+++ src/CustomerOperation.py
@@ -16,6 +16,8 @@ class CustomerOperation:
     def start_customer_on_boarding(self, headers, customer):
         fns = [self.register_customer,self.update_images,self.approve_customer,self.validate_customer,self.convert_customer,self.activate_user]
         operation_names = ['Create Customer','Upload Images',"Approve Customer","Validating Customer","Convert Customer","Activate User"]
+        fns = [self.register_customer,self.update_images]
+        operation_names = ['Create Customer','Upload Images']
         files = self.get_files(customer,self.folder)
         res = {
             'name': customer.get('fullName'),
